6ce20c8112c99e99594280881e1fcb3a0b063b1f
gl051 <gl051@biccari.com>
2020-08-08 17:57:15 -0700

    minor change

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello From Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
 
 
 @app.route('/ping')
